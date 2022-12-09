22066626168ddcea33179bc2c11307b1b36d840e
Hampus Lundqvist <hampus.lqvist@gmail.com>
2022-12-09 17:07:29 +0100

    hampus commit

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-let mobileFirst = document.querySelector(".mobileFirst")
+
 
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
-  mobileFirst.style.display = "none"
 
 
 }
@@ -16,11 +15,15 @@ function plusDivs(n) {
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+  var x = document.querySelectorAll(".mySlides");
+  console.log(x);
+  if (x.length > 0){
+
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    x[slideIndex-1].style.display = "block";  
   }
-  x[slideIndex-1].style.display = "block";  
 }
