be3c8fd37cc4c6398ece31b24ac4748e9217edbb
Hampus Lundqvist <hampus.lqvist@gmail.com>
2022-12-12 19:18:59 +0100

    hampus commit

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
-
-
 function toggleMobileMenu(menu) {
-    menu.classList.toggle('open');
-
-
+  menu.classList.toggle("open");
 }
 
 var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-  showDivs(slideIndex += n);
+  showDivs((slideIndex += n));
 }
 
 function showDivs(n) {
   var i;
   var x = document.querySelectorAll(".mySlides");
-  console.log(x);
-  if (x.length > 0){
-
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length}
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";  
+  if (x.length > 0) {
+    if (n > x.length) {
+      slideIndex = 1;
     }
-    x[slideIndex-1].style.display = "block";  
+    if (n < 1) {
+      slideIndex = x.length;
+    }
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
   }
 }
+
+
+let addProduct = document.querySelector(".addProduct")
+// let buttonBuy = document.querySelector(".buttonBuy")
+
+
+let y = 1
+function myFunction() {
+  addProduct.textContent = y++
+};
